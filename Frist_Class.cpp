68f0047a2b981e682259b5c85c353036f4069b59
sultanxdev <sultancodess@gmail.com>
2026-01-11 20:42:16 +0530

    starting oop

---
+++ Frist_Class.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+using namespace std:
+
+class Teacher{
+
+  private:
+
+ double salary;
+
+public:
+
+ string name;
+
+ int age;
+
+ string department;
+
+ // member function
+
+ function change_dept(String change_dept)
+ {
+  department=change_dept;
+
+ }
+
+ void function setsal(double sal){
+
+  salary= sal
+
+ }
+ void function getsal(){
+  return salary
+ }
+
+
+
+
+};
+int main(){
+  Teacher t1;
+  t.name="sultan"
+  cout<<t1.name;
+}
