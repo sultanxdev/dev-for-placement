61baa1a42b87aed5396b1ff6b18c050c76a7dc6e
sultanxdev <sultancodess@gmail.com>
2026-01-15 01:14:45 +0530

    Refactor code structure for improved readability and maintainability

---
+++ pattern03.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  /*
+  aaaaaa
+  bbbbbb
+  cccccc
+  */
+  int row,col;
+  for(row=1;row<=5;row++)
+
+  {
+    char name ='a'+row-1;
+    for(col=1;col<=5;col++)
+    {
+      cout<<name,"";
+    }
+    cout<<endl;
+  }
+}
---
+++ pattern05.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+/*
+1
+22
+333
+4444*/
+{
+  int row,col;
+  for(row=5;row>=1;row--)
+  {
+    for(col=row;col>=1;col--)
+    {
+      cout<<row,"";
+      
+    }
+    cout<<endl;
+
+  }
+}
